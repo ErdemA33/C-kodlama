9af3ab19425ba672ee346fa16ddcfd241ee15c70
Erdem AYDIN <erdemaydinn32@gmail.com>
2025-10-31 00:18:33 +0300

    Create p2_10.c

---
+++ p2_10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+        int a,b,c,d,e;
+        double ort;
+        scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+        ort =(a+b+c+d+e) / 5;
+        printf("%lf", ort);
+        return 0;
+}
