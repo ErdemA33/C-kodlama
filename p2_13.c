5d60a0e2609cb3888233b3a3dc4488c730450953
Erdem AYDIN <erdemaydinn32@gmail.com>
2025-10-31 22:50:47 +0300

    Create p2_13.c

---
+++ p2_13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+        int metre,kisi,t1,t2,t3,t4,t5;
+        scanf("%d%d", &metre,&kisi);
+        scanf("%d",&t1);
+        scanf("%d",&t2);
+        scanf("%d",&t3);
+        scanf("%d",&t4);
+        scanf("%d",&t5);
+        printf("%d", kisi/metre - t1);
+        printf("%d", kisi/metre - t2);
+        printf("%d", kisi/metre - t3);
+        printf("%d", kisi/metre - t4);
+        printf("%d", kisi/metre - t5);
+        return 0;
+
+
+
+
+}
+~      
