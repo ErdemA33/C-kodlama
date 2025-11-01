cd12c9df632b691c68c4c7f644fccb5df2257687
Erdem AYDIN <erdemaydinn32@gmail.com>
2025-11-01 15:47:40 +0300

    Add files via upload

---
+++ p2_21.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int x;
+	scanf("%d", &x);
+
+	switch(x){
+		case 1:printf("bir"); break;
+                case 2:printf("iki"); break;
+                case 3:printf("üç"); break;
+                case 4:printf("dört"); break;
+                case 5:printf("beş"); break;
+                case 6:printf("altı"); break;
+                case 7:printf("yedi"); break;
+                case 8:printf("sekiz"); break;
+                case 9:printf("dokuz"); break;
+                case 10:printf("on"); break;
+	
+	}
+	return 0;
+}
